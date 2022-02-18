c6448976e3a759e269c904e6d35dd6c94b578341
SarthakBansal <74849415+sarthakbansalgit@users.noreply.github.com>
2022-02-18 05:41:53 +0530

    Update gay.py

--- gay.py
+++ gay.py
@@ -6,9 +6,9 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://gitHub.com/DEADLY-FIGHTERS/DEADLY-SPAM-BOT && cd DEADLY-SPAM-BOT && python3 -m OfficialSameer")
+system("git clone https://github.com/sarthakbansalgit/spambots && cd spambots && python3 -m OfficialSameer")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @chitchatters23")
 # Script written for stop heroku suspension
 # use with credit else gay
-#By @MdNoor786 (t.me/Md_Noor786) For Fixing Heroku Suspension
+#By @mrsarthakzz (t.me/mrsarthakzz) For Fixing Heroku Suspension
